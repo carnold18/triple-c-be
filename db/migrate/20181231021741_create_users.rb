312b2f0f12b299f41b0c750d5895da905fd3dc32
Christopher Arnold <chrisarnold2010@gmail.com>
2018-12-30 21:24:50 -0600

    adding local files

---
+++ db/migrate/20181231021741_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email_address
+      t.string :zip_code
+      t.string :password
+      t.string :dob
+
+      t.timestamps
+    end
+  end
+end
