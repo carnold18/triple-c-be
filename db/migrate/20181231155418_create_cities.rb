b13a94a2cea0285166562098c31bb0b5bd313c9d
Christopher Arnold <chrisarnold2010@gmail.com>
2018-12-31 10:30:28 -0600

    added rows to city table

--- db/migrate/20181231155418_create_cities.rb
+++ db/migrate/20181231155418_create_cities.rb
@@ -1,7 +1,18 @@
 class CreateCities < ActiveRecord::Migration[5.2]
   def change
     create_table :cities do |t|
-      t.string :type
+      t.boolean :residential
+      t.boolean :commercial
+      t.integer :residential_electric_use
+      t.integer :residential_electric_expenditure
+      t.integer :residential_gas_use
+      t.integer :residential_gas_expenditure
+      t.integer :commercial_electric_use
+      t.integer :commercial_electric_expenditure
+      t.integer :commercial_gas_use
+      t.integer :commercial_gas_expenditure
+      t.integer :city_fuel_use_diesel
+      t.integer :city_fuel_use_gas
       t.string :zip_code
       t.string :name
 
